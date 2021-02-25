1921fa55c8281ae6458c9869b4d56d7463fe7ece
James Wu <james_wu@appdet.com>
2021-02-25 20:18:33 +0800

    + node interface and class and linkedlist interface. ! todo: linkedlist class

---
+++ LinkedList/_LinkedList.ts
@@ -0,0 +1,41 @@
+export interface INode<T> {
+  value: T
+  prev: INode<T> | null
+  next: INode<T> | null
+}
+
+export interface ILinkedList<T> {
+  head: Node<T>
+  length: number
+  insertAt(node: Node<T>, value: number): boolean
+  removeAt(value: number): Node<T> | null
+  toString(): string
+}
+
+export class Node<T> implements INode<T> {
+  constructor(
+    public value: T,
+    public prev: INode<T> | null = null,
+    public next: INode<T> | null = null
+  ) {}
+}
+
+export class LinkedList<T> implements ILinkedList<T> {
+  public head!: Node<T>
+  public length: number = 1
+  constructor(value: T) {
+    this.head = new Node<T>(value)
+  }
+
+  insertAt(node: Node<T>, value: number): boolean {
+    return true
+  }
+
+  removeAt(index: number): Node<T> | null {
+    return null
+  }
+
+  toString(): string {
+    return ''
+  }
+}
