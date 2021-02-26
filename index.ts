9fe6a5a1b6d5ebd4198c7b253c7a7283f4c6fbe8
James Wu <james_wu@appdet.com>
2021-02-26 11:55:33 +0800

    + overload test

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import chalk from 'chalk'
 
 // linkedlist practice
-console.group(chalk.rgb(216,112,147)('Linkedlist'))
+console.group(chalk.bgRgb(216,112,147)(' Linkedlist '))
 import './LinkedList'
+console.groupEnd()
+
+console.log(chalk.bgGreenBright('\n                           \n'))
+
+console.group(chalk.bgRgb(216,112,147)(' Overload '))
+import './Overload'
 console.groupEnd()
